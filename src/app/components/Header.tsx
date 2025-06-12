9c75af2bf34c2283e3d3873d17b8bb358a143b4b
Ravi <ravi.k@imsolutions.mobi>
2025-06-12 17:54:52 +0530

    commit

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from "next/link";
+
 export default function Header() {
     return (
         <div className="nav-black_wrap">
@@ -7,62 +9,62 @@ export default function Header() {
                 <div className="padding-global">
                     <div className="container-large w-container">
                         <div className="nav">
-                            <a href="/" aria-current="page" className="nav-logo-link w-nav-brand w--current">
+                            <Link href="/" aria-current="page" className="nav-logo-link w-nav-brand w--current">
                                 <svg className="nav-logo" xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 143 70" fill="none">
                                     {/* SVG Content */}
                                     <path d="M66.6261 60.0781C66.8039 60.0484..." fill="currentColor" />
                                 </svg>
-                            </a>
+                            </Link>
 
                             <nav role="navigation" className="nav-menu w-nav-menu">
-                                <a href="/" className="nav-link_wrap w-inline-block w--current">
+                                <Link href="/" className="nav-link_wrap w-inline-block w--current">
                                     <div className="nav-link-icon w-embed">
                                         <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <circle cx="4" cy="4" r="4" fill="#a1c889" />
                                         </svg>
                                     </div>
                                     <div className="nav-link">Home</div>
-                                </a>
-                                <a href="/biodiversity" className="nav-link_wrap w-inline-block">
+                                </Link>
+                                <Link href="/biodiversity" className="nav-link_wrap w-inline-block">
                                     <div className="nav-link-icon w-embed">
                                         <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <circle cx="4" cy="4" r="4" fill="#a1c889" />
                                         </svg>
                                     </div>
                                     <div className="nav-link">Biodiversity</div>
-                                </a>
-                                <a href="/amenities" className="nav-link_wrap w-inline-block">
+                                </Link>
+                                <Link href="/amenities" className="nav-link_wrap w-inline-block">
                                     <div className="nav-link-icon w-embed">
                                         <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <circle cx="4" cy="4" r="4" fill="#a1c889" />
                                         </svg>
                                     </div>
                                     <div className="nav-link">Amenities</div>
-                                </a>
-                                <a href="/location" className="nav-link_wrap w-inline-block">
+                                </Link>
+                                <Link href="/location" className="nav-link_wrap w-inline-block">
                                     <div className="nav-link-icon w-embed">
                                         <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <circle cx="4" cy="4" r="4" fill="#a1c889" />
                                         </svg>
                                     </div>
                                     <div className="nav-link">Location</div>
-                                </a>
-                                <a href="/contact" className="nav-link_wrap w-inline-block">
+                                </Link>
+                                <Link href="/contact" className="nav-link_wrap w-inline-block">
                                     <div className="nav-link-icon w-embed">
                                         <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <circle cx="4" cy="4" r="4" fill="#a1c889" />
                                         </svg>
                                     </div>
                                     <div className="nav-link">Contact</div>
-                                </a>
-                                <a href="/gallery" className="nav-link_wrap w-inline-block">
+                                </Link>
+                                <Link href="/gallery" className="nav-link_wrap w-inline-block">
                                     <div className="nav-link-icon w-embed">
                                         <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <circle cx="4" cy="4" r="4" fill="#a1c889" />
                                         </svg>
                                     </div>
                                     <div className="nav-link">Gallery</div>
-                                </a>
+                                </Link>
                             </nav>
 
                             <div className="nav-menu_mobile w-nav-button">
