2819bad9eb42936c75431c90e954e5cd9a6d9b5c
lokeshkushwah <lokesh.kushwah@gmail.com>
2025-09-03 13:43:12 +0530

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,18 +22,6 @@ export default function RootLayout({ children }) {
         <link rel="shortcut icon" href="/images/favicon.png" type="image/x-icon" />
         <link rel="apple-touch-icon" href="/images/webclip.svg" />
         <PathBasedPreload />
-        {/* Preload critical CSS */}
-        <link rel="preload" href="/css/components.css" as="style" />
-        <link rel="preload" href="/css/style.css" as="style" />
-
-        {/* Load non-critical CSS without blocking render */}
-        <link rel="stylesheet" href="/css/components.css" media="print" onLoad="this.media='all'" />
-        <link rel="stylesheet" href="/css/style.css" media="print" onLoad="this.media='all'" />
-
-        {/* Bootstrap & FontAwesome lazy load */}
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" media="print" onLoad="this.media='all'" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" media="print" onLoad="this.media='all'" />
-
       </head>
       <body>
         <ClientWrapper>
