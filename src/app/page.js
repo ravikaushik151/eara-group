668b0c6713ac24ebe142891af7a86b0c870980b0
lokeshim <lokesh@imsolutions.mobi>
2025-10-29 16:13:16 +0530

    remove comited code

--- src/app/page.js
+++ src/app/page.js
@@ -19,122 +19,12 @@ export const metadata = {
 };
 
 export default function Home() {
-  /*  useEffect(() => {
-      if (typeof window !== "undefined" && window.Lenis) {
-        const lenis = new window.Lenis({
-          duration: 1.2,
-          easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-          direction: "vertical",
-          gestureDirection: "vertical",
-          smooth: true,
-          mouseMultiplier: 1,
-          smoothTouch: false,
-          touchMultiplier: 2,
-          infinite: false,
-        });
-  
-        const getCurrentPath = () => window.location.pathname;
-        const setLerp = () => {
-          lenis.options.lerp =
-            getCurrentPath() === "/being-here-at-studio" ? 0.1 : undefined;
-          console.log("lerp applied:", lenis.options.lerp);
-          console.log("Current Path:", getCurrentPath());
-        };
-  
-        setTimeout(setLerp, 100);
-  
-        function raf(time) {
-          lenis.raf(time);
-          requestAnimationFrame(raf);
-        }
-        requestAnimationFrame(raf);
-      }
-  
-      const form = document.getElementById(
-        "wf-form-Melodies-of-Life-Contact-Form"
-      );
-      const trigger = document.getElementById("brochure-download-trigger");
-      const popup = document.getElementById("popup-form");
-      const thankYou = document.getElementById("popup-thankyou");
-      const sourceInput = document.getElementById("SourceURL");
-  
-      if (sourceInput) {
-        sourceInput.value = window.location.href;
-      }
-  
-      if (trigger && popup) {
-        trigger.addEventListener("click", (e) => {
-          e.preventDefault();
-          popup.style.display = "flex";
-        });
-      }
-  
-      const closePopup = () => {
-        if (popup) popup.style.display = "none";
-        if (thankYou) thankYou.style.display = "none";
-      };
-  
-      if (form) {
-        form.addEventListener("submit", (e) => {
-          e.preventDefault();
-          fetch(form.action, {
-            method: form.method,
-            body: new FormData(form),
-          })
-            .then((res) => {
-              if (res.ok) {
-                closePopup();
-                document.getElementById("brochure-download-link")?.click();
-                if (thankYou) thankYou.style.display = "flex";
-              } else {
-                alert(
-                  "There was an issue with your submission. Please try again."
-                );
-              }
-            })
-            .catch((err) => console.error("Submission error:", err));
-        });
-      }
-    }, []); */
+
 
   return (
     <>
       <main className="main-wrapper">
-        {/* <div className="overflow-clip">
 
-          <div className="overflow-hidden position-relative">
-            <div className="position-relative text-white d-flex align-items-md-center align-items-end" style={{ height: '100vh' }}>
-
-              <Image
-                src='/images/hero-image-edit-4-new.avif'
-                alt='banner'
-                fill
-                priority
-                fetchPriority="high"
-                sizes="100vw"
-                className="object-cover z-n1 "
-
-              />
-
-
-              <div className="container position-relative z-1">
-                <div className="d-flex my-md-0 my-5 py-md-0 py-4">
-                  <div>
-                    <h1 className="display-4 fw-bold mb-3 w-100 text-center">Luxury reimagined in green. <br />Welcome to EARA Group</h1>
-
-                    <h4 className="mb-3 fw-light">{``}</h4>
-
-                    <p className="mb-4 lead text-center w-100 text-success">In a home kissed by trees and wrapped in serenity, <br /> you don&rsquo;t just live - you thrive</p>
-                  </div>
-                  <Link href='#' className="btn btn-outline-light px-4 py-2 d-none  text-center">
-                    Explore More
-                  </Link>
-                </div>
-              </div>
-
-            </div>
-          </div>
-        </div> */}
         <div className="overflow-clip" id="main-slider">
           <div id="carouselExampleDark" className="header-section position-relative">
             <div className="row">
@@ -147,8 +37,6 @@ export default function Home() {
                     mobilePoster="/images/mobile-image.avif"
                     className="rounded-0"
                   />
-                 
-
 
                 </div>
               </div>
@@ -183,84 +71,12 @@ export default function Home() {
                   </div>
 
                 </div>
-
               </div>
-
-
             </div>
 
           </section>
 
-          {/* <section className="same-gap postion-relative bg-light d-none">
-            <div className="container-fluid">
-              <div id="project" className="noclass" data-bs-ride="carousel">
-                <div className="row">
-                  <div className="title text-center mb-3">
-                    <h2 className="text-black mb-4">Ongoing Projects</h2>
-                  </div>
-                </div>
-                <div className="row d-flex align-content-center justify-content-center px-md-3">
-                  <div className="col-md-5 text-center">
-                    <img
-                      src="/images/Ongoing-Project.jpg"
-                      alt="ongoing"
-                      className="img-fluid  mb-3"
-                    />
-                  </div>
-                  <div
-                    className="col-md-5 ps-md-5 d-flex align-content-center align-items-center"
-                    data-aos="fade-left"
-                    data-aos-duration={1500}
-                  >
-                    <div>
-                      <div className="title text-left">
-                        <h4 className=" text-black mb-2 fs-4 m-center" style={{ fontSize: "24px" }}>Amidst Nature</h4>
-                        <p>
-                          <span className="text-black">
-                            <i
-                              className="fa fa-map-marker mx-1"
-                              aria-hidden="true"
-                            />
-                            Sompura fermentum Lorem - consectetur risus
-                          </span>
-                        </p>
-                      </div>
-                      <div className="noclass">
-                        <p className="text-justify m-center lh text-black mt-3">
-                          Spread across 15 lush acres in Kaggalipura, just Off Kanakapura Main  Road, this premium plotted development invites you to design your forever home in a place where the city fades and nature speaks. Thoughtfully approved by BMRDA and connected to every urban convenience, this is more than a plot — it&rsquo;s a path back to balance.
-                        </p>
-                      </div>
-                      <div className="row align-items-center mb-3 mt-4 mob-center">
-                        <div className="col-md-6 col-12 border-end border-dark m-center">
-                          <h4 className="bhk text-black fs-5 mb-2">
-                            ₹7999/sq.ft.
-                          </h4>
-                          <div className="text-black m-center">
-                            <h6>launch price — exclusive, value-driven investment</h6>
-                          </div>
-                        </div>
-                        <div className="col-md-6 col-12 px-5 text-black m-center">
-                          <h6 className="mb-2">15 acres</h6>
-                          <h6 className="mb-2">1500 – 3300 sq.ft.</h6>
-                          <h6 className="mb-0">BMRDA Approved</h6>
-                        </div>
-                      </div>
 
-                      <p className="m-center py-md-0 py-3">
-                        <a
-                          href="/amidstnature"
-                          className="btn btn-dark text-white border"
-                          style={{ fontSize: "15px !important" }}
-                        >
-                          More Details
-                        </a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section> */}
 
           <section className="same-gap position-relative bg-light theme-bg-dark up-coming" style={{
             backgroundImage: "url('/images/Flower_Cream.avif')",
