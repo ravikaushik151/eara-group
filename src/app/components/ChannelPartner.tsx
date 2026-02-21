1140788e3514978ba3f9d573dc304deff38f0456
lokesh <lokesh@imsolutions.mobi>
2026-02-21 13:11:27 +0530

    updates

--- src/app/components/ChannelPartner.tsx
+++ src/app/components/ChannelPartner.tsx
@@ -192,7 +192,7 @@ export default function ChannelPartner() {
                             </div>
                         </div> */}
                         <div className="col-lg-6 p-4">
-                            <div className="px-3 px-md-4 py-4  rounded shadow-sm ">
+                            <div className="  rounded-2  ">
                                 {/* <h5 className="text-uppercase mb-0 theme-color-dark text-center" style={{ color: '#282563' }}>
                                     Partner With Us
                                 </h5>
